f8bbd85f345278889f82a018d2a889857fae0e84
Frank HL CHI <frankhlchi@163.com>
2017-01-21 23:30:46 +0800

    Update Step 1 Exploratory data analysis and binning.R

--- Step 1 Exploratory data analysis and binning.R
+++ Step 1 Exploratory data analysis and binning.R
@@ -1,15 +1,16 @@
-require(caret)
+library(caret)
 library(smbinning)
+library(ggplot2)
 
 #load the data
-german_credit <- read.csv("~/german_credit2.csv")
+german_credit <- read.csv("~/german_credit.csv")
 train <-createDataPartition(y=german_credit$Creditability,p=0.75,list=FALSE)
 train2 <- german_credit[train, ]
 test2 <- german_credit[-train, ]
 
 #Explore data distribution 
-ggplot(german_credit, aes(x = duration,y = ..count..,)) + geom_histogram(fill = "blue", colour = "grey60", size = 0.2, alpha = 0.2,binwidth = 5)
-ggplot(german_credit, aes(x = Amount,y = ..count..,)) + geom_histogram(fill = "blue", colour = "grey60", size = 0.2, alpha = 0.2,binwidth = 1000)
+ggplot(german_credit, aes(x = Duration,y = ..count..,)) + geom_histogram(fill = "blue", colour = "grey60", size = 0.2, alpha = 0.2,binwidth = 5)
+ggplot(german_credit, aes(x = CreditAmount,y = ..count..,)) + geom_histogram(fill = "blue", colour = "grey60", size = 0.2, alpha = 0.2,binwidth = 1000)
 ggplot(german_credit, aes(x = Age,y = ..count..,)) + geom_histogram(fill = "blue", colour = "grey60", size = 0.2, alpha = 0.2,binwidth = 5)
 ggplot(german_credit, aes(x =Creditability,y = ..count..,)) + geom_histogram(fill = "blue", colour = "grey60" , alpha = 0.2,stat="count")
 
@@ -21,3 +22,4 @@ Ageresult=smbinning(df=train2,y="Creditability",x="Age",p=0.05)
 smbinning.plot(CreditAmountresult,option="WoE",sub="CreditAmount") 
 smbinning.plot(Durationresult,option="WoE",sub="Duration")
 smbinning.plot(Ageresult,option="WoE",sub="Age")
+
